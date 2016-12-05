dfd8f9430424e8c26ae7dcf1f856f5c3863390eb
Michael Hedges <mhedges@Michaels-MacBook-Pro.local>
2016-12-05 14:07:39 -0600

    implement date-picker

---
+++ src/Few/Fields/DatePicker.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mhedges
+ * Date: 12/5/16
+ * Time: 2:04 PM
+ */
+
+namespace Few\Fields;
+
+class DatePicker extends Field
+{
+    use CommonMethods;
+}
---
+++ src/templates/date.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mhedges
+ * Date: 12/5/16
+ * Time: 2:05 PM
+ */
+
+return [
+    'key' => '',
+    'label' => '',
+    'name' => '',
+    'type' => 'date_picker',
+    'instructions' => '',
+    'required' => 0,
+    'conditional_logic' => 0,
+    'wrapper' => [
+        'width' => '',
+        'class' => '',
+        'id' => '',
+    ],
+    'display_format' => 'd/m/Y',
+    'return_format' => 'd/m/Y',
+    'first_day' => 1,
+];
